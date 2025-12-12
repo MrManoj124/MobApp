963f0f52b02bee10b30f868c1d81f8946d19607f
MrManoj124 <manojmanorooban@gmail.com>
2025-12-12 21:57:32 +0530

    Just simply Start a code

--- unisports/app/index.tsx
+++ unisports/app/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hey Manorooban Let's Crack it...!</Text>
     </View>
   );
 }
