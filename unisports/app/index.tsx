eca3e6e52b6e1cd2d1a6d28cf92201c8cc9dcc29
MrManoj124 <manojmanorooban@gmail.com>
2025-12-13 17:40:46 +0530

    Update the index.tsx file

--- unisports/app/index.tsx
+++ unisports/app/index.tsx
@@ -1,16 +1,5 @@
-import { Text, View } from "react-native";
-
+//Index 
+import Register from './Register';
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Hey Manorooban Let's Crack it...!</Text>
-    </View>
-  );
+  return <Register />;
 }
