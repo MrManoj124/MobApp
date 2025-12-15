689ee6671ed983bd40ba62964ef8c6e756e8946c
MrManoj124 <manojmanorooban@gmail.com>
2025-12-15 22:08:08 +0530

    Create a Login function for Login.tsx file

--- unisports/app/Login.tsx
+++ unisports/app/Login.tsx
@@ -3,3 +3,29 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import React, {useState} from "react";
 import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {auth} from "../firebaseConfig";
+
+export default funtion Login(){
+    const [email, setEmail]= useState('');
+    const [password, setPassword]=useState('');
+
+    const handleLogin = async () => {
+        if(!email ||!password){
+            alert("Please fill sll the fields");
+            return;
+        }
+        try{
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      console.log("Logged in user:", user);
+      alert("Login successful!");
+
+      
+      router.push("/Home"); 
+    } catch (error: any) {
+      console.log(error);
+      alert(error.message); 
+    }
+  };
+
+}
